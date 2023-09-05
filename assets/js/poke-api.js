04c7be707adaea27d1e1555d87af9929f2bb5b78
AlanoNovaes <109998485+AlanoNovaes@users.noreply.github.com>
2023-09-04 22:58:26 -0300

    Comitt para envio da pasta Pokedex

---
+++ assets/js/poke-api.js
@@ -0,0 +1,43 @@
+
+const pokeApi = {}
+
+function convertPokeApiDetailToPokemon(pokeDetail) {
+    const pokemon = new Pokemon()
+    pokemon.number = pokeDetail.id
+    pokemon.name = pokeDetail.name
+
+    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
+    const [type] = types
+
+    pokemon.types = types
+    pokemon.type = type
+
+    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
+
+    const abilities = pokeDetail.abilities.map((ability) => ability.ability)
+    pokemon.abilities = abilities.map((ability) => ability.name)
+
+    const stats = pokeDetail.stats.map((stat) => stat)
+    pokemon.stats = stats;
+
+    return pokemon
+}
+
+pokeApi.getPokemonDetail = (pokemon) => {
+    return fetch(pokemon.url)
+        .then((response) => response.json())
+        .then(convertPokeApiDetailToPokemon)
+}
+
+pokeApi.getPokemons = (offset = 0, limit = 5) => {
+    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+    return fetch(url)
+        .then((response) => response.json())
+        .then((jsonBody) => jsonBody.results)
+        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+        .then((detailRequests) => Promise.all(detailRequests))
+        .then((pokemonsDetails) => {
+            return pokemonsDetails
+        })
+}
